f6e9c4467a3c331981fbecd16496349f81b74f73
maxpel10 <maxi.101997@gmail.com>
2018-05-31 10:23:12 -0300

    No lo se

---
+++ Detectorhtml/src/detectorhtml/Error.java
@@ -0,0 +1,31 @@
+package detecciondeerrores;
+/**
+ *
+ * @author Facundo Decena
+ * @author Maximiliano Pellegrino
+ */
+public class Error {
+    private int linea;
+    private String comentario;
+
+    public Error(int l, String c) {
+        linea = l;
+        comentario = c;
+    }
+    
+    public int get_num_linea(){
+        return linea;
+    }
+    
+    public String get_comentario(){
+        return comentario;
+    }
+    
+    public void set_comentario(String c){
+        comentario = c;
+    }
+    public void set_num_linea(int l){
+        linea = l;
+    }
+    
+}
